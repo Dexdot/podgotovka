b514e1f32dea3e391c3897c149dfa563c063335e
hvxzcb <dexdot@MacBook-Pro-Kamil.local>
2021-06-29 21:55:32 +0300

    Lesson types dropdown

--- components/LessonEdit/Dropdowns.tsx
+++ components/LessonEdit/Dropdowns.tsx
@@ -5,10 +5,18 @@ import { Dropdown, DropdownItem } from '@/components/common/Dropdown/Dropdown';
 
 import { LessonEditContext } from '@/store/lesson-edit';
 import { useCourses } from '@/api/hooks/courses/useCourses';
+import { LessonType } from '@/types/lessons';
 
 import cls from './LessonEdit.module.scss';
 
 const filters = {};
+const types: DropdownItem[] = [
+  { id: 'webinar', text: 'Вебинар' },
+  { id: 'examwork', text: 'Пробник' },
+  { id: 'extra_lesson', text: 'Дополнительное занятие' },
+  { id: 'psychologist', text: 'Вебинар с психологом' },
+  { id: 'motivation', text: 'Мотивационный вебинар' }
+];
 
 export const Dropdowns: React.FC = observer(() => {
   const store = useContext(LessonEditContext);
@@ -41,6 +49,21 @@ export const Dropdowns: React.FC = observer(() => {
     setCourseID(Number(v.id));
   };
 
+  // Type
+  const { type, setType } = store;
+
+  const [typeUI, setTypeUI] = useState<DropdownItem>();
+
+  useEffect(() => {
+    const finded = types.find((t) => t.id === type) || types[0];
+    setTypeUI(finded);
+  }, [type]);
+
+  const onTypeChange = async (v: DropdownItem) => {
+    const t = v.id as LessonType;
+    setType(t);
+  };
+
   return (
     <ul className={cls.dropdowns}>
       {course && (
@@ -50,6 +73,11 @@ export const Dropdowns: React.FC = observer(() => {
             value={course}
             onChange={onCourseChange}
           />
+        </li>
+      )}
+      {typeUI && (
+        <li>
+          <Dropdown items={types} value={typeUI} onChange={onTypeChange} />
         </li>
       )}
     </ul>
