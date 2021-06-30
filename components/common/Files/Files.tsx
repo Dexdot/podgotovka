05d6318acdb1d9f6871eb9b0cb6e3838eef7190c
Kamil <kamil@startted.com>
2021-06-30 10:43:13 +0300

    Upload files to lesson

--- components/common/Files/Files.tsx
+++ components/common/Files/Files.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
 import React, { useEffect, useRef, useState } from 'react';
 
 import { FileI } from '@/types/common';
 import { AddButton } from '@/components/common/AddButton/AddButton';
 import { FilesAPI } from '@/api/common/upload';
 import { showAlert } from '@/utils/network';
+import { bytesToSize } from '@/utils/format';
 
 import cls from './Files.module.scss';
 import { FileIcon, RemoveIcon } from './icons';
@@ -47,18 +51,34 @@ export const Files: React.FC<Props> = ({ files, onChange }) => {
     }
   };
 
+  const removeFile = (file: FileI) => {
+    setFiles((prevFiles) =>
+      prevFiles.filter((f) => f.file_link !== file.file_link)
+    );
+  };
+
   return (
     <div>
       <ul className={cls.list}>
-        {files.map((f) => (
+        {fs.map((f) => (
           <li key={f.file_link}>
             <div className={cls.file}>
+              <a
+                href={f.file_link}
+                className={cls.link}
+                target="_blank"
+                rel="noreferrer"
+              />
               <FileIcon />
               <div>
                 <p className={cls.name}>{f.name}</p>
-                <p className={cls.size}>{f.size}</p>
+                <p className={cls.size}>{bytesToSize(f.size)}</p>
               </div>
-              <button className={cls.remove} type="button">
+              <button
+                className={cls.remove}
+                type="button"
+                onClick={() => removeFile(f)}
+              >
                 <RemoveIcon />
               </button>
             </div>
--- utils/format.ts
+++ utils/format.ts
@@ -8,3 +8,15 @@ export function numberWithSpaces(x: number): string {
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
   return parts.join('.');
 }
+
+export function bytesToSize(bytes: number, decimals = 2): string {
+  if (bytes === 0) return '0 байтов';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['байтов', 'КБ', 'МБ', 'ГБ', 'ТБ', 'ПБ', 'ЭБ', 'ЗБ', 'ЙБ'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+}
